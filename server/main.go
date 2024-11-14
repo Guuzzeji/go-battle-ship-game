6f6316d168014d7bf572abcf14c8360ffd818463
Guuzzeji <5lh8dl440f@gmail.com>
2024-11-14 07:48:36 -0800

    Create temp server code

--- server/main.go
+++ server/main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var addr = flag.String("addr", "localhost:8080", "http service address")
+var upgrader = websocket.Upgrader{}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = conn.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("start", *addr)
+	flag.Parse()
+	http.HandleFunc("/ws", websocketHandler)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
